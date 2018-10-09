86c6f5ac6052fdcf1ecbad10fb186abc5b985fea
unknown <sundrie42@gmail.com>
2018-10-09 13:32:55 +0200

    Exercice fini (partie 2 ch.2)

--- src/com/company/Order.java
+++ src/com/company/Order.java
@@ -21,16 +21,33 @@ public class Order {
     // Affiche le menu choisi par l'utilisateur
     // le nbMenu (paramètre de fonction) correspond au chiffre du menu
     public void displaySelectedMenu(int nbMenu){
-        if (nbMenu == 1){
-            System.out.println("Vous avez choisi le  menu "+ menu1);
-        } else if (nbMenu == 2){
-            System.out.println("Vous avez choisi le  menu "+ menu2);
-        } else if (nbMenu == 3){
-            System.out.println("Vous avez choisi le  menu "+ menu3);
-        } else{
-            System.out.println("Votre saisie n'est pas correcte. Veuillez réessayer");
+
+        // En utilisant if else if et else
+//        if (nbMenu == 1){
+//            System.out.println("Vous avez choisi le  menu "+ menu1);
+//        } else if (nbMenu == 2){
+//            System.out.println("Vous avez choisi le  menu "+ menu2);
+//        } else if (nbMenu == 3){
+//            System.out.println("Vous avez choisi le  menu "+ menu3);
+//        } else{
+//            System.out.println("Votre saisie n'est pas correcte. Veuillez réessayer");
+//        }
+
+        // En utilisant un switch
+        switch (nbMenu){
+            case 1 :
+                System.out.println("Vous avez choisi le  menu "+ menu1);
+                break; // break sert à arrêter le switch donc la boucle
+            case 2 :
+                System.out.println("Vous avez choisi le  menu "+ menu2);
+                break;
+            case 3 :
+                System.out.println("Vous avez choisi le  menu "+ menu3);
+                break;
+            default : // default c'est pour si aucun case n'a fonctionné (équivalent de else) donc indispensable pour un retour utilisateur
+                System.out.println("Votre saisie n'est pas correcte. Veuillez réessayer");
+                break;
         }
-        // System.out.println("Vous avez choisi le menu "+nbMenu);
 
     }
 }
