0c29e857b9d7c0b22e360f035d20c58e1c6eb77c
unknown <sundrie42@gmail.com>
2018-10-09 12:07:40 +0200

    Début de l'apprentissage de java avec openclassroom

---
+++ src/com/company/Order.java
@@ -0,0 +1,16 @@
+package com.company;
+
+// Le nom du fichier (.java) doit être identique à celui de cette class
+public class Order {
+
+    // Cette fonction va afficher les menus dans le Terminal
+    public void showMenu(){
+
+    }
+
+    // Affiche le menu choisi par l'utilisateur
+    // le nbMenu (paramètre de fonction) correspond au chiffre du menu
+    public void showUserMenuSelected(int nbMenu){
+
+    }
+}
