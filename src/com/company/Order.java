b5fa72c0416eff13f51ce00ed76abcd0aae2183e
unknown <sundrie42@gmail.com>
2018-10-09 13:22:42 +0200

    Exercice conditions fait

--- src/com/company/Order.java
+++ src/com/company/Order.java
@@ -3,12 +3,14 @@ package com.company;
 // Le nom du fichier (.java) doit être identique à celui de cette class
 public class Order {
 
+    // Les variables des différents menu, je les déclare ici pour que displaySelectedMenu() puisse les utiliser
+    String menu1 = "Poulet";
+    String menu2 = "Boeuf";
+    String menu3 = "Vegan";
+
     // Cette fonction va afficher les menus dans le Terminal
     public void displayAvailableMenu(){
         System.out.println("Choix Menu");
-        String menu1 = "Poulet";
-        String menu2 = "Boeuf";
-        String menu3 = "Vegan";
         // Le + est pour concaténer
         System.out.println("1 - " + menu1);
         System.out.println("2 - " + menu2);
@@ -19,6 +21,16 @@ public class Order {
     // Affiche le menu choisi par l'utilisateur
     // le nbMenu (paramètre de fonction) correspond au chiffre du menu
     public void displaySelectedMenu(int nbMenu){
-        System.out.println("Vous avez choisi le menu "+nbMenu);
+        if (nbMenu == 1){
+            System.out.println("Vous avez choisi le  menu "+ menu1);
+        } else if (nbMenu == 2){
+            System.out.println("Vous avez choisi le  menu "+ menu2);
+        } else if (nbMenu == 3){
+            System.out.println("Vous avez choisi le  menu "+ menu3);
+        } else{
+            System.out.println("Votre saisie n'est pas correcte. Veuillez réessayer");
+        }
+        // System.out.println("Vous avez choisi le menu "+nbMenu);
+
     }
 }
